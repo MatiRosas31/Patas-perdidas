02b7839e4a69e3205c7b046818e71062efc0d17f
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-12-24 13:21:01 -0300

    Initialize icono-mapa-flor

---
+++ src/front/js/layout.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
+import { BackendURL } from "./component/backendURL";
+import Map from "./component/Map";
+import { Home } from "./pages/home";
+import { Demo } from "./pages/demo";
+import { Single } from "./pages/single";
+import injectContext from "./store/appContext";
+import  User  from "./component/user";
+import  Login  from "./component/login";
+import  Signup  from "./component/signup";
+
+
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/footer";
+import NewPetLost from "./pages/newPetLost.js";
+import NewFoundPet from "./pages/newFoundPet.js"; 
+import UbicationMap from "./component/ubication_map.js";
+
+import PetView from "./component/PetView.jsx";
+import PetCard from "./component/PetCard.jsx";
+
+
+//create your first component
+const Layout = () => {
+    //the basename is used when your project is published in a subdirectory and not in the root of the domain
+    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+    const basename = process.env.BASENAME || "";
+
+    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+
+    return (
+        <div>
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <Navbar />
+                    <Routes>
+                        <Route element={<Home />} path="/" />                        
+                        <Route element={<Map />} path="/map" />  
+                        <Route element ={<NewPetLost />} path="/newpetlost" />
+                        <Route element = {<NewFoundPet/>} path = "/newpetfound"/>
+                        <Route element = {<UbicationMap/>} path = "/ubicationmap"/>
+                        <Route element={<Demo />} path="/demo" />
+                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<User />} path="/user" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Signup />} path="/signup" />
+                        <Route element={<PetView/>} path="/petview"/>
+                        <Route element= {<PetCard/>} path="/petcard/:theid"/>
+                        <Route element={<h1>Not found!</h1>} />
+                    </Routes>
+                </ScrollToTop>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default injectContext(Layout);
---
+++ src/front/js/pages/home.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+import "../../styles/home.css";
+import "../../styles/home.css";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div>
+			<div
+				className="d-flex justify-content align-items-center"
+				style={{
+					height: "90vh",
+					position: "relative",
+					overflow: "hidden",
+				}}
+			>
+				<div className="container  z-3" >
+					<h1 className=" adlam-display-regular fw-bolder fs-4 mb-3">
+						Una ayuda para encontrar mascotas perdidas en tu zona.
+					</h1>
+					<p className="adlam-display-minusc text-muted fs-5 mb-4">
+						Encuentra a tu mascota o ayuda a otra persona a encontrarla. Miles
+						de historias felices nos respaldan.
+					</p>
+					<div className=" row d-flex justify-content">
+						<Link to = "/newpetlost">
+						<button className="btn btn-danger col-sm-3 rounded-pill btn-md shadow">Perdí a mi mascota</button>
+						</Link>
+						<div className="w-100 mt-3"></div>
+						<Link to="/newpetfound">
+						<button className="btn btn-success col-sm-3 rounded-pill btn-md shadow">Encontré una mascota</button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
